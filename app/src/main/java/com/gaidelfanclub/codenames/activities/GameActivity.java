a87ed74b7c36700c7921bbe7f051790112d8f792
Bezrukov Denis <debez086@gmail.com>
2017-01-31 22:43:27 +0400

    removed RecyclerView, added fixed gridview

--- app/src/main/java/com/gaidelfanclub/codenames/activities/GameActivity.java
+++ app/src/main/java/com/gaidelfanclub/codenames/activities/GameActivity.java
@@ -2,24 +2,28 @@ package com.gaidelfanclub.codenames.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.GridLayout;
 
 import com.gaidelfanclub.codenames.BaseActivity;
 import com.gaidelfanclub.codenames.R;
 import com.gaidelfanclub.codenames.card.Word;
+import com.gaidelfanclub.codenames.card.WordHolder;
 import com.gaidelfanclub.codenames.card.WordStore;
 import com.gaidelfanclub.codenames.card.WordType;
-import com.gaidelfanclub.codenames.card.WordsAdapter;
 import com.gaidelfanclub.codenames.utils.GenUtils;
 import com.gaidelfanclub.codenames.utils.KeywordUtils;
-import com.gaidelfanclub.codenames.utils.KeywordsStore;
 
 import java.util.Random;
 
 public class GameActivity extends BaseActivity {
+
+    private static final int DIMEN = 5;
 
     private static final String EXTRA_LEADER = "EXTRA_LEADER";
     private static final String EXTRA_KEYWORD = "EXTRA_KEYWORD";
@@ -33,9 +37,7 @@ public class GameActivity extends BaseActivity {
         return intent;
     }
 
-    private RecyclerView recyclerView;
-    private TextView keyView;
-
+    private GridLayout gridLayout;
 
     private String keyword;
     private boolean isLeader;
@@ -45,6 +47,7 @@ public class GameActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_game);
+        gridLayout = (GridLayout) findViewById(R.id.container);
         isLeader = getIntent().getBooleanExtra(EXTRA_LEADER, true);
         keyword = getIntent().getStringExtra(EXTRA_KEYWORD);
         if (savedInstanceState == null) {
@@ -52,27 +55,57 @@ public class GameActivity extends BaseActivity {
         } else {
             words = (Word[]) savedInstanceState.getParcelableArray(TAG_WORDS);
         }
+        initGrid();
+    }
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler);
-        keyView = (TextView) findViewById(R.id.words);
-        initRecycler(recyclerView);
+    private void initGrid() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        size.y -= getStatusBarHeight();
+
+        int[] horizontalSpacing = getSpacing(size.x, DIMEN);
+        int[] verticalSpacing = getSpacing(size.y, DIMEN);
+
+        for (int i = 0; i < DIMEN; i++) {
+            for (int j = 0; j < DIMEN; j++) {
+                View view = LayoutInflater.from(this).inflate(R.layout.i_word, null);
+                WordHolder holder = new WordHolder(view, isLeader);
+                holder.bind(words[i * DIMEN + j]);
+                GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+                param.height = verticalSpacing[i];
+                param.width = horizontalSpacing[j];
+                param.setGravity(Gravity.CENTER);
+                param.columnSpec = GridLayout.spec(j);
+                param.rowSpec = GridLayout.spec(i);
+                gridLayout.addView(view, param);
+            }
+        }
+    }
+
+    private int[] getSpacing(int available, int count) {
+        int alreadyUsed = 0;
+        int[] spacing = new int[count];
+        for (int i = 0; i < count; i++) {
+            spacing[i] = (available - alreadyUsed) / (count - i);
+            alreadyUsed += spacing[i];
+        }
+        return spacing;
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putParcelableArray(TAG_WORDS, words);
-    }
-
-    private void initRecycler(RecyclerView recyclerView) {
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 5));
-        WordsAdapter adapter = new WordsAdapter(isLeader);
-        adapter.setWords(words);
-
-        if (isLeader) {
-            keyView.setText("Слово ведущего: " + keyword + "\n Слово игроков: " + KeywordsStore.getInstance().getWord(getSeed()));
-        }
-        recyclerView.setAdapter(adapter);
     }
 
     private Word[] createWords() {
